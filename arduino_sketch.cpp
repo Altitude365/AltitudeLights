f811a0a52be75112ab4159941858224dcd189a9b
Jon Jander <jon.jander@lanmat.se>
2018-03-06 16:03:47 +0100

    fix

--- arduino_sketch.cpp
+++ arduino_sketch.cpp
@@ -67,97 +67,15 @@ int extra, extra2;
 
 void loop()
 {
-    //clear(0xff0000, 3000);
-    //delay(1000);
+    clear(0xff0000, 800);
+    delay(1000);
 
-    for (int i=0; i < 250 ;i++){ 
-        extra = random(-30,40);
-        extra2 = random(-83,40);
-        pixels.setPixelColor(random(0, 2700), (36 + extra), (83 + extra2), (214 + extra));
-    }
-    pixels.show();
-}
-
-
-
-
-u32 colorWeightedAvg(u32 color1, u32 color2, u8 weight)
-{
-    u8 r1, g1, b1, r2, g2, b2;
-    colorPackedToScalar(&r1, &g1, &b1, color1);
-    colorPackedToScalar(&r2, &g2, &b2, color2);
-    r1 = channelWeightedAvg(r1, r2, weight);
-    g1 = channelWeightedAvg(g1, g2, weight);
-    b1 = channelWeightedAvg(b1, b2, weight);
-    return pixels.Color(r1, g1, b1);
-}
-
-u8 channelWeightedAvg(u8 a, u8 b, u8 weight)
-{
-    u16 aa = a * (256 - weight);
-    u16 bb = b * weight;
-    return (aa + bb) >> 8;
-}
-
-void addPixelColor(u16 pixelPos, u32 addColor)
-{
-    u32 oldColor = pixels.getPixelColor(pixelPos);
-    u32 newColor = additiveColorMix(oldColor, addColor);
-    pixels.setPixelColor(pixelPos, newColor);
-}
-
-u32 additiveColorMix(u32 color1, u32 color2)
-{
-    u8 r1, g1, b1, r2, g2, b2;
-    colorPackedToScalar(&r1, &g1, &b1, color1);
-    colorPackedToScalar(&r2, &g2, &b2, color2);
-    r1 = colorChannelClampedAdd(r1, r2);
-    g1 = colorChannelClampedAdd(g1, g2);
-    b1 = colorChannelClampedAdd(b1, b2);
-    u32 c = pixels.Color(r1, g1, b1);
-    return c;
-}
-
-void colorPackedToScalar(u8* r, u8* g, u8* b, u32 color) {
-    *b = color;
-    color >>= 8;
-    *g = color;
-    color >>= 8;
-    *r = color;
-}
-
-u8 colorChannelClampedAdd(u8 a, u8 b) {
-    u16 c = a + b;
-    if (c > 255) {
-        c = 255;
-    }
-    return c;
-}
-
-void xmasRedGreenTwinkles(u16 runSec)
-{
-    const u8 flashOnDelayMs = 20;
-    const u8 flashPauseMs = 150;
-    const u8 numTwinklesPerSwap = 3;
-    bool redOrGreenFirst = false;
-    u32 startMs = millis();
-    u8 numTwinklesToNextSwap = 0;
-    while (millis() < startMs + runSec * 1000UL) {
-        if (!numTwinklesToNextSwap--) {
-            numTwinklesToNextSwap = numTwinklesPerSwap;
-            redOrGreenFirst = !redOrGreenFirst;
-        }
-        u8 twinkleLedIdx = random(0, pixels.numPixels() - 1);
-        pixels.setPixelColor(twinkleLedIdx, 0xffffff);
-        pixels.show();
-        delay(flashOnDelayMs);
-        for (u8 i = 0; i < pixels.numPixels(); ++i) {
-            u32 c = (i + redOrGreenFirst) & 1 ? 0xff0000 : 0x00ff00;
-            pixels.setPixelColor(i, c);
-        }
-        pixels.show();
-        delay(flashPauseMs);
-    }
+    //for (int i=0; i < 250 ;i++){ 
+    //    extra = random(-30,40);
+    //    extra2 = random(-83,40);
+    //    pixels.setPixelColor(random(0, 2700), (36 + extra), (83 + extra2), (214 + extra));
+    //}
+    //pixels.show();
 }
 
 void clear(u32 color, u16 clearMs)
@@ -169,33 +87,6 @@ void clear(u32 color, u16 clearMs)
             pixels.show();
             delay(delayMs);
         }
-    }
-}
-
-s16 wrap(s16 val, s16 maxVal)
-{
-    if (val < 0) {
-        return val + maxVal;
-    }
-    else if (val >= maxVal) {
-        return val - maxVal;
-    }
-    else {
-        return val;
-    }
-}
-
-u16 wrapAdd(s16 val, s16 addVal, s16 maxVal)
-{
-    s16 t = val + addVal;
-    if (t > maxVal) {
-        return t - maxVal - 1;
-    }
-    else if (t < 0) {
-        return maxVal + t + 1;
-    }
-    else {
-        return t;
     }
 }
 
