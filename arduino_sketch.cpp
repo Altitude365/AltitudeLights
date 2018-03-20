f32ef928191e55c546370b95471d66b70c2a7bf2
Jon Jander <jon.jander@lanmat.se>
2018-03-20 08:12:45 +0100

    test new example

--- arduino_sketch.cpp
+++ arduino_sketch.cpp
@@ -1,67 +1,39 @@
+//plz dont change this part
 #include <Adafruit_NeoPixel.h>
 #include <inttypes.h>
 
 #define NEOPIXEL_PIN 1
-
-// We use "pixel" to refer to a single WS2812B package, which contains three LEDs (red, green and blue).
-
-// Select number of pixels.
 #define NUM_PIXELS 2700
-
-// We copy some type name defines here to keep the sketch self contained.
 #define u8 uint8_t
 #define u16 uint16_t
 #define u32 uint32_t
 #define s16 int16_t
-
-// Create the NeoPixel driver object. On the PC, this becomes the emulator.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-// This must be below the short int type names and the NeoPixel library init.
-#ifndef Arduino_h
-#include "arduino_sketch.hpp"
-// When running on the PC, we include stdio.h for printf() debugging.
-#include <stdio.h>
-#endif
-
-// Example of wrapping stuff that will only compile in the IDE.
-#ifdef Arduino_h
-void debugMarker()
-{
-  pinMode(1, OUTPUT);
-  for (u8 i = 0; i < 10; ++i) {
-    digitalWrite(1, HIGH);
-    delay(500);
-    digitalWrite(1, LOW);
-    delay(500);
-  }
-}
-#endif
-
-// End of boilerplate.
 
 void setup()
 {
-// Arduino_h is only defined when compiling the sketch in the Arduino IDE.
-#ifndef Arduino_h
-    // Select line, circle or grid pixel layout for emulator.
-    // pixels.setPixelLayout(Strip);
-    pixels.setPixelLayout(Ring);
-    // pixels.setPixelLayout(Grid);
-#endif
     pixels.begin();
-    // setBrightness() is intended for use only in setup() and not for animations. It's ignored by the emulator (see
-    // the README).
     pixels.setBrightness(50);
     pixels.show();
 }
 
+//Alittude functions
+int pixelsgetPixelColor(int px) {return pixels.getPixelColor(px);}
+void stripsSetPixelColor(int px, byte r ,byte g , byte b) {pixels.setPixelColor(px, r, g, b);}
+void stripsSetPixelColor(int px, int color) {pixels.setPixelColor(px, color);}
+void stripsShow() {pixels.show();}
+//End of altitude function 
 
-struct Segment {
-  u32 color;
-  s16 speed;
-  u8 lengthPercent;
-};
+/**
+ * 
+ * Please use stripsShow, pixelsgetPixelColor and stripsSetPixelColor,
+ * instead of pixels.setPixelColor and pixels.getPixelColor.
+ * We will change these functions in the final test to a modified version.
+ * We do this because we are using 3 stripes instead on one. 
+ * 
+ **/
+
+//Edit example below
 
 int extra, extra2;
 
